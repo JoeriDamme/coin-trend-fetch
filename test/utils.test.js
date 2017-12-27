d2d2f6a526db402848a148474e96aff07b556a52
Joeri Damme <joeri.damme@kpn.com>
2017-12-28 00:23:03 +0100

    mocha and travis

---
+++ test/utils.test.js
@@ -0,0 +1,9 @@
+const { expect } = require('chai');
+
+describe('Util tests', () => {
+  describe('utils/mongoose-uri.js', () => {
+    it('should create valid string with no username and password', () => {
+      expect(1 + 1).to.equal(2);
+    });
+  });
+});
